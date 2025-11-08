55555ea5526e8c92494f31e9ba9a78b02fac8d0f
Chriq <christiancook52@gmail.com>
2025-11-08 01:55:46 -0500

    troll

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,20 +1,20 @@
 using Godot;
 using System;
 
-public partial class MainMenu : Control
-{
-    public override void _Ready()
-    {
-        ((Label)GetChild(1)).Text = SaveData.Instance.data["TOTAL_SCORE"].ToString()    ;
+public partial class MainMenu : Control {
+    [Export] AudioStreamPlayer player;
+    [Export] Label scoreLabel;
+
+    public override void _Ready() {
+        scoreLabel.Text = SaveData.Instance.data["TOTAL_SCORE"].ToString();
+        player.Play();
     }
 
-    public void Menu_StartGame()
-    {
+    public void Menu_StartGame() {
         GameController.Instance.StartGame();
     }
 
-    public void Menu_QuitGame()
-    {
+    public void Menu_QuitGame() {
         GetTree().Quit();
     }
 }
