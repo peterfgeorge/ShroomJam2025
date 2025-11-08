0fea0124b4d8bc6a6b3f436a11ece9eb84fef234
Chriq <christiancook52@gmail.com>
2025-11-08 16:43:25 -0500

    remove jj signal

--- Scripts/JetpackJoyride/JJPlayerController.cs
+++ Scripts/JetpackJoyride/JJPlayerController.cs
@@ -30,7 +30,7 @@ public partial class JJPlayerController : CharacterBody2D {
     }
 
     private void Timeout() {
-        GD.Print("timeout");
+        GameController.Instance.GameTimerTimeout -= Timeout;
         GameController.Instance.PassGame(currentScore);
     }
 }
