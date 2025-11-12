b12772c56d6cb0d504c06d05d622d86b8755cc37
peterfgeorge <petegeo52@gmail.com>
2025-11-12 14:38:46 -0500

    Adds in collectibles and enemies to jetpack joyride

--- Scripts/JetpackJoyride/ObstacleSpawner.cs
+++ Scripts/JetpackJoyride/ObstacleSpawner.cs
@@ -1,9 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class ObstacleSpawner : Node {
     [Export] public PackedScene obstaclePrefab;
-    [Export] public PackedScene collectiblePrefab;
+    [Export] public PackedScene[] collectiblePrefabs = Array.Empty<PackedScene>();
 
     private const float SPAWN_DELAY_MIN = 0.2f;
     private const float SPAWN_DELAY_MAX = 1.5f;
@@ -44,9 +45,15 @@ public partial class ObstacleSpawner : Node {
                 float y = rng.RandfRange(30, 150);
                 Vector2 spawnPos = new Vector2(x, y);
 
-                Node2D obstacle = collectiblePrefab.Instantiate<Node2D>();
-                obstacle.GlobalPosition = spawnPos;
-                AddChild(obstacle);
+                if (collectiblePrefabs != null && collectiblePrefabs.Length > 0) {
+                    int idx = (int)rng.RandiRange(0, collectiblePrefabs.Length - 1);
+                    PackedScene chosen = collectiblePrefabs[idx];
+                    if (chosen != null) {
+                        Node2D collectible = chosen.Instantiate<Node2D>();
+                        collectible.GlobalPosition = spawnPos;
+                        AddChild(collectible);
+                    }
+                }
 
                 spawnTimer = 0f;
                 ResetCollDelay();
